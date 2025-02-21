803a7cb64e4b082ed8730a68fcc6e84d585b5c13
lonearvin <outlookddal@gmail.com>
2025-02-21 14:19:39 +0800

    完成

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,10 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
-	"os"
 	"time"
 )
 
@@ -20,11 +21,36 @@ func GenerateJWT(username string) (string, error) {
 		"exp":      time.Now().Add(time.Hour * 72).Unix(),
 	})
 
-	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	signedToken, err := token.SignedString([]byte("secret"))
 	return "Bearer " + signedToken, err
 }
 
 func CheckPassword(input string, password string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(password), []byte(input))
 	return err == nil
+}
+
+func ParseJWT(tokenString string) (string, error) {
+	if len(tokenString) > 7 && tokenString[:7] == "Bearer " {
+		tokenString = tokenString[7:]
+	}
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username, ok := claims["username"].(string)
+		if !ok {
+			return "", errors.New("invalid token")
+		}
+		return username, nil
+	}
+	return "", errors.New("invalid token")
 }
