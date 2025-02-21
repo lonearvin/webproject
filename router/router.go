e9998bc7a3a66d57274485bed7b6f0bd84742535
lonearvin <outlookddal@gmail.com>
2025-02-21 14:17:39 +0800

    完成汇率的创建和登录部分，完成JWT解析部分

--- router/router.go
+++ router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"webproject/controllers"
+	"webproject/middlewares"
 )
 
 func SetRouter() *gin.Engine {
@@ -11,6 +12,12 @@ func SetRouter() *gin.Engine {
 	{
 		auth.POST("/login", controllers.Login)
 		auth.POST("/register", controllers.Register)
+	}
+	api := r.Group("/api/")
+	api.GET("/exchangeRate", controllers.GetExchangeRates)
+	api.Use(middlewares.AuthMiddleWare())
+	{
+		api.POST("/exchangeRate", controllers.CreateExchangeRate)
 	}
 	return r
 }
