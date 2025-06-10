0ec933a52f9545021c98ff74e6ac0f3a9696a9d3
lonearvin <outlookddal@gmail.com>
2025-06-10 22:17:50 +0800

    refactor: 修复utils脚本的bug

--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,10 @@ import (
 // HandleContactPost 实现的功能是，接收数据，然后使用异步去发送邮件
 func HandleContactPost(data ContactPostData) error {
 
+	if err := global.GlobalDB.AutoMigrate(&data); err != nil {
+		return err
+	}
+
 	if err := global.GlobalDB.Create(&data).Error; err != nil {
 		return err
 	}
