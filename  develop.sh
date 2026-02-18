611be27b650ed4c4917bcbdad6e3f2bf3568073b
lonearvin <outlookddal@gmail.com>
2026-02-18 09:41:20 +0800

    feat: 添加开发部署脚本用于构建和运行Docker镜像

---
+++  develop.sh
@@ -0,0 +1,15 @@
+
+# 打包镜像
+sudo docker build -t webproject:latest .
+
+# 打标签
+sudo docker tag webproject:latest 192.168.1.12:19001/webproject/webproject:latest
+
+# 推送镜像
+sudo docker push 192.168.1.12:19001/webproject/webproject:latest
+
+# 拉取镜像，部署
+sudo docker pull 192.168.1.12:19001/webproject/webproject:latest
+
+# 部署容器
+sudo docker run -d --name webproject -p 8080:8080 192.168.1.12:19001/webproject/webproject:latest
