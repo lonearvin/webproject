05ea4f5da61254a8531767209f1c1d1bf1f33c4f
lonearvin <outlookddal@gmail.com>
2026-02-18 09:43:16 +0800

    fix(部署脚本): 添加容器挂载卷和环境变量配置
    添加必要的挂载卷配置以便持久化应用数据，并设置GIN_MODE为release模式
    同时配置容器自动重启策略

---  develop.sh
+++  develop.sh
@@ -12,4 +12,12 @@ sudo docker push 192.168.1.12:19001/webproject/webproject:latest
 sudo docker pull 192.168.1.12:19001/webproject/webproject:latest
 
 # 部署容器
-sudo docker run -d --name webproject -p 8080:8080 192.168.1.12:19001/webproject/webproject:latest
+sudo docker run -d --name webproject \
+  -p 8080:8080 \
+  -v $(pwd)/config:/app/config \
+  -v $(pwd)/templates:/app/templates \
+  -v $(pwd)/static:/app/static \
+  -v $(pwd)/log:/app/log \
+  -e GIN_MODE=release \
+  --restart unless-stopped \
+  192.168.1.12:19001/webproject/webproject:latest
